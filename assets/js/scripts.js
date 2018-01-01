aa5fc5c0a79fb01c14bde3057b2eac781baf8549
zeeshan-ks <zeeshan.ali@kalsym.com>
2018-01-01 10:59:44 +0500

    a

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -236,19 +236,6 @@ jQuery(function ($) {
     // -------------------------------------------------------------
     (function () {
     
-    /* Vimeo API: http://developer.vimeo.com/player/js-api */
-    
-        var iframe = document.getElementById('nofocusvideo');
-        // $f == Froogaloop
-        var player = $f(iframe);
-
-        $('.modal').on('hidden.bs.modal', function () {
-        player.api('pause');
-        })
-
-        $('.modal').on('shown.bs.modal', function () {
-        player.api('play');
-        })
     }());
 
 
